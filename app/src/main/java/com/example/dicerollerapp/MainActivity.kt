13b4f0e435fae728f5f1fa7a11a9114ec2b8c435
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-12-11 17:48:27 +0100

    WIP

--- app/src/main/java/com/example/dicerollerapp/MainActivity.kt
+++ app/src/main/java/com/example/dicerollerapp/MainActivity.kt
@@ -10,44 +10,45 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity(), OnItemSelectedListener {
 
     @SuppressLint("MissingInflatedId")
-    private var dice : Dice = Dice()
+    private var dice: Dice = Dice()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.rollButton)
         val diceSpinner: Spinner = findViewById(R.id.diceSpinner)
-        ArrayAdapter.createFromResource(this,R.array.choice_of_dice,
-        android.R.layout.simple_spinner_item).also { adapter ->
+        ArrayAdapter.createFromResource(
+            this, R.array.choice_of_dice,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
             adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line)
             diceSpinner.adapter = adapter
         }
         diceSpinner.onItemSelectedListener = this
         rollButton.setOnClickListener {
-            Toast.makeText(this, "Die D${dice.numberOfFaces} rolled!", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "D${dice.numberOfFaces} rolled!", Toast.LENGTH_SHORT).show()
             rollDice()
         }
 
     }
 
-
     private fun rollDice() {
         val rollResult = dice.roll()
         val resultTextView: TextView = findViewById(R.id.throwResult)
-        resultTextView.text = rollResult.toString()
+        resultTextView.text = "Rolling ..."
+        resultTextView.text = rollResult.toString() + rollResult.toString()
     }
 
     override fun onItemSelected(p0: AdapterView<*>?, view: View?, position: Int, id: Long) {
-        val diceMaximumValue : Long = id+1;
+        val diceMaximumValue: Long = id + 1;
         dice.numberOfFaces = diceMaximumValue
     }
 
     override fun onNothingSelected(p0: AdapterView<*>?) {
-        TODO("Not yet implemented")
+
     }
 
 }
-
 
 
 class Dice() {
