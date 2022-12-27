bb390869c16cb4bf9f568dc48a33afdca6cc5df2
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-12-27 11:30:38 +0100

    Roll several dice of the same kind.

--- app/src/main/java/com/example/dicerollerapp/MainActivity.kt
+++ app/src/main/java/com/example/dicerollerapp/MainActivity.kt
@@ -6,17 +6,21 @@ import android.view.View
 import android.widget.*
 import android.widget.AdapterView.OnItemSelectedListener
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity(), OnItemSelectedListener {
 
     @SuppressLint("MissingInflatedId")
     private var dice: Dice = Dice()
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.rollButton)
         val diceSpinner: Spinner = findViewById(R.id.diceSpinner)
+        val numberOfDice: EditText = findViewById(R.id.numberOfDiceInput)
+
         ArrayAdapter.createFromResource(
             this, R.array.choice_of_dice,
             android.R.layout.simple_spinner_item
@@ -27,12 +31,23 @@ class MainActivity : AppCompatActivity(), OnItemSelectedListener {
         diceSpinner.onItemSelectedListener = this
         rollButton.setOnClickListener {
             Toast.makeText(this, "D${dice.numberOfFaces} rolled!", Toast.LENGTH_SHORT).show()
-            rollDice()
+            val dices: Int = Integer.parseInt(numberOfDice.text.toString())
+            rollManyDice(dices)
+            //rollSingleDie()
         }
 
     }
 
-    private fun rollDice() {
+    private fun rollManyDice(numberOfDice: Int) {
+        val diceRolls = IntArray(numberOfDice)
+        for(i in diceRolls.indices)
+            diceRolls[i] += dice.roll().toInt()
+        val resultTextView: TextView = findViewById(R.id.throwResult)
+        resultTextView.text = diceRolls.joinToString()
+        //resultTextView.text ="2";
+    }
+
+    private fun rollSingleDie() {
         val rollResult = dice.roll()
         val resultTextView: TextView = findViewById(R.id.throwResult)
         resultTextView.text = rollResult.toString()
